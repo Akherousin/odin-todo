894cdda085eba4f923d19f328d231f77b7bb26f0
Arthur Badin <akherousin94@gmail.com>
2022-09-11 18:47:22 +0600

    add htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[main][contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -35,4 +36,11 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
 };
