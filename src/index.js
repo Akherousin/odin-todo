7c1d7fc7cdebf25b77bfd7e1f3f646863d1fec75
Arthur Badin <akherousin94@gmail.com>
2022-09-11 18:38:10 +0600

    add babel and sass

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.scss';
+
+console.log('hey');
+const hey = 'something';
+console.log(hey);
+const hello = 'hey';
+console.log(hello);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // babel
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
+      //scss
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
   },
 };
